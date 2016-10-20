b03b4f38ae9452030a280a49ede8414a9761e9b4
Chloe Eghtebas <nceghtebas@hotmail.com>
2016-10-20 18:02:36 +0200

    Adding base unity project and gitignore

---
+++ Zoom/Assets/SelectObject.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SelectObject : MonoBehaviour {
+
+	// private SubscriberSocket m_subscriber;
+	// private NetMQContext m_context;
+
+	// Use this for initialization
+	void Start () {
+		// ForceDotNet.Force(); 
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
